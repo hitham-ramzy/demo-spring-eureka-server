f7eec0a79f116d1c8c6d7ace8b91352989508716
hitham <hitham.ramzy@mondiamedia.com>
2018-12-23 22:51:33 +0200

    Adding AWS configurations

--- src/main/java/com/alten/euricaservice/EuricaServiceApplication.java
+++ src/main/java/com/alten/euricaservice/EuricaServiceApplication.java
@@ -1,16 +1,29 @@
 package com.alten.euricaservice;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EuricaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EuricaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EuricaServiceApplication.class, args);
+    }
 
+    @Bean
+    @Profile("!default")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean instanceConfigBean = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        instanceConfigBean.setDataCenterInfo(info);
+        return instanceConfigBean;
+    }
 }
 
